3c3c2f81a5c168ec88787bd94505ae4635924b8b
sanjanaaa17 <duttasanjana17@gmail.com>
2024-09-18 22:30:49 +0530

    changes againn

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,25 @@ dotenv.config();
 // Create express app
 const app = express();
 
-var corsOptions = {
-  origin: process.env.FRONTEND_URL,
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
-// Middleware
-app.use(cors(corsOptions));
+const prodOrigins = [process.env.FRONTEND_URL];
+console.log(process.env.FRONTEND_URL);
+const allowedOrigins = prodOrigins;
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error(`${origin} not allowed by cors`));
+      }
+    },
+    optionsSuccessStatus: 200,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+//Middleware
 app.use(express.json());
 app.use(cookieParser());
 
@@ -28,8 +41,7 @@ dbConnection();
 const authRoute = require("./routes/authRoutes");
 const userRoute = require("./routes/userRoutes");
 const problemRoute = require("./routes/problemRoutes");
-const submissionRoute = require("./routes/submissionRoutes"); 
-
+const submissionRoute = require("./routes/submissionRoutes");
 
 // Public routes
 app.use("/auth", authRoute);
@@ -38,7 +50,6 @@ app.use("/profile", userRoute);
 // Protected routes
 app.use("/problems", problemRoute);
 app.use("/submissions", submissionRoute);
-
 
 // Start/activate the server
 const PORT = process.env.PORT || 7000;
