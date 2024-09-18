6f3a625845c49c816d91499ba077563c4ddb1341
sanjanaaa17 <duttasanjana17@gmail.com>
2024-09-18 22:20:24 +0530

    changes again

--- backend/compiler/server.js
+++ backend/compiler/server.js
@@ -13,7 +13,6 @@ var corsOptions = {
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
-
 //middlewares
 app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,12 @@ dotenv.config();
 // Create express app
 const app = express();
 
+var corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 // Middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
